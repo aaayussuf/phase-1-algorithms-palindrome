5521c6902f525c39495e5de414a3eb1a63cfd1f0
Abdullahi Abdikadir <aabdikadir849@gmail.com>
2025-01-01 22:55:12 +0300

    Completed palindrome assignment

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// Function to check if a string is a palindrome
+function isPalindrome(str) {
+  // Reverse the string and compare it to the original
+  return str === str.split('').reverse().join('');
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+// Test cases
+console.log(isPalindrome("madam"));  // true
+console.log(isPalindrome("robot"));  // false
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("level"));  // true
+console.log(isPalindrome("hello"));  // false
